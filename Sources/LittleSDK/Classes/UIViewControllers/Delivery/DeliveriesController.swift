2ed298d6db922f6c9222928652274daf444ac1dd
Boaz James <boaz.otieno@little.africa>
2025-03-10 10:56:17 +0300

    Fix presenting viewcontroller

--- Sources/LittleSDK/Classes/UIViewControllers/Delivery/DeliveriesController.swift
+++ Sources/LittleSDK/Classes/UIViewControllers/Delivery/DeliveriesController.swift
@@ -163,7 +163,10 @@ public class DeliveriesController: UIViewController, UITableViewDataSource, UITa
     }
     
     @objc func backHome() {
-        navigationController?.dismiss(animated: true)
+//        navigationController?.dismiss(animated: true)
+        navigationController?.willMove(toParent: nil)
+        navigationController?.view.removeFromSuperview()
+        navigationController?.removeFromParent()
     }
     
     // MARK: - Search
